baffd1712d83e94164555036ee950aa416745456
Nicolas Mermoud-Ghraichy <nicolas.mermoud-ghraichy@epfl.ch>
2025-05-01 16:52:53 +0200

    Set custom 'pages' configuration

--- observablehq.config.js
+++ observablehq.config.js
@@ -6,15 +6,17 @@ export default {
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
+  pages: [
+    {
+      name: "Examples",
+      pages: [
+        {name: "Dashboard", path: "/example-dashboard"},
+        {name: "Report", path: "/example-report"},
+        {name: "Weather", path: "/weather"},
+        {name: "Cadastre Rénové", path: "/cadastre-renove"},
+      ]
+    }
+  ],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
